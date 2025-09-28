5bf1cec209ec32818ce0e2fa1f913480d3157d8c
Amit <ajamitjana098@gmail.com>
2025-09-28 19:57:41 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let songIndex=0;
+let audioElement=new Audio('saiyaraa.mp3')
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+
+let songs=[
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
+    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"}
+]
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity=0;
+    }
+})
+audioElement.addEventListener('timeupdate', ()=>{
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100)
+    myProgressBar.value=progress;
+})
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime=myProgressBar.value * audioElement.duration/100;
+})
