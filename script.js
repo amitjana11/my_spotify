0ce7148b254edfa0844afe8c1879af969d58ee9c
Amit <ajamitjana098@gmail.com>
2025-09-29 12:26:40 +0530

    Updated homepage design

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let songIndex=0;
-let audioElement=new Audio('saiyaraa.mp3')
+let audioElement=new Audio('songs/1.mp3.mp3')
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
 let gif=document.getElementById('gif');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
 
 let songs=[
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"},
-    {songName: "Saiyaraa", filePath: "saiyaraa.mp3", coverPath: "cover_image.jpg"}
+    {songName: "Afsos", filePath: "songs/1.mp3.mp3", coverPath: "covers/1.jpg.jpeg"},
+    {songName: "Phele bhi main", filePath: "songs/2.mp3.mp3", coverPath: "covers/2.jpg.jpg"},
+    {songName: "Co2", filePath: "songs/3.mp3.mp3", coverPath: "covers/3.jpg.jpeg"},
+    {songName: "Die with a smile", filePath: "songs/4.mp3.mp3", coverPath: "covers/4.jpg.jpeg"},
+    {songName: "Jhol", filePath: "songs/5.mp3.mp3", coverPath: "covers/5.jpg.jpg"},
+    {songName: "Kahani Suno", filePath: "songs/6.mp3.mp3", coverPath: "covers/6.jpg.jpg"},
+    {songName: "Mitte de dibbe", filePath: "songs/7.mp3.mp3", coverPath: "covers/7.jpg.jpg"},
+    {songName: "Pal pal", filePath: "songs/8.mp3.mp3", coverPath: "covers/8.jpg.jpg"},
+    {songName: "Saiyaraa", filePath: "songs/9.mp3.mp3", coverPath: "covers/9.jpg.jpeg"},
+    {songName: "Tu hai kahan", filePath: "songs/10.mp3.mp3", coverPath: "covers/10.jpg.jpg"}
 ]
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+});
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
@@ -36,4 +41,53 @@ audioElement.addEventListener('timeupdate', ()=>{
 })
 myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime=myProgressBar.value * audioElement.duration/100;
+})
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+    element.classList.add('fa-circle-play');
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        songIndex=parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src=`songs/${songIndex}.mp3.mp3`;
+        masterSongName.innerText=songs[songIndex-1].songName;
+        audioElement.currentTime=0;
+        audioElement.play();
+        gif.style.opacity=1;
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    })
+})
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=10){
+        songIndex=0;
+    }
+    else{
+        songIndex+=1;
+    }
+    audioElement.src=`songs/${songIndex}.mp3.mp3`;
+    masterSongName.innerText=songs[songIndex-1].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+})
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=1){
+        songIndex=0;
+    }
+    else{
+        songIndex-=1;
+    }
+    audioElement.src=`songs/${songIndex}.mp3.mp3`;
+    masterSongName.innerText=songs[songIndex-1].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
 })
